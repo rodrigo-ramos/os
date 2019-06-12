b0e2e03847d1f87908b355bc3b072b20afa5fa7c
rodrigo-ramos <rodrigo.ramos.go@gmail.com>
2019-06-12 13:17:07 -0500

    Admin message for testing commits into Jenkins 5

--- sayhello.py
+++ sayhello.py
@@ -2,8 +2,6 @@
 
 print("This is my code running into OpenShift")
 print ("testing GitHubs plugin webhook for building.")
-print ("test again")
-print ("test again2")
 print ("test again3")
 print ("test again4")
 
